ab64475499ea663e2f421ce7ddca72c8ee238dc6
LalanaChami <lalanachamika123@gmail.com>
2020-04-18 11:22:04 +0530

    Reorganize backend routes #backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const Supplier = require('./models/supplier');
+const supplierRoutes = require('./routes/supplier');
+
+
 mongoose.connect('mongodb+srv://lalana:OJx2X4IllVNl9up4@cluster0-rjtww.mongodb.net/pharmacy?retryWrites=true&w=majority',{useNewUrlParser: true , useUnifiedTopology: true})
   .then(()=>{
     console.log('connected to database!');
@@ -33,66 +35,66 @@ app.use((req,res,next)=>{
 });
 
 
-app.post("/api/supplier",(req,res,next)=>{
-const supplier = new Supplier({
-  supplierID: req.body.supplierID,
-  name: req.body.name,
-  email: req.body.email,
-  contact: req.body.contact,
-  drugsAvailable: req.body.drugsAvailable
-});
-supplier.save().then(createdSupplier=>{
-res.status(201).json({
-  message:'Supplier Added Successfully',
-  supplierId : createdSupplier._id
-});
+// app.post("/api/supplier",(req,res,next)=>{
+// const supplier = new Supplier({
+//   supplierID: req.body.supplierID,
+//   name: req.body.name,
+//   email: req.body.email,
+//   contact: req.body.contact,
+//   drugsAvailable: req.body.drugsAvailable
+// });
+// supplier.save().then(createdSupplier=>{
+// res.status(201).json({
+//   message:'Supplier Added Successfully',
+//   supplierId : createdSupplier._id
+// });
 
-});
+// });
 
-});
+// });
 
-app.put("/api/supplier/:id", (req,res,next)=>{
-  const supplier = new Supplier({
-    _id: req.body.id,
-    supplierID: req.body.supplierID,
-    name: req.body.name,
-    email: req.body.email,
-    contact: req.body.contact,
-    drugsAvailable: req.body.drugsAvailable
-  });
-  Supplier.updateOne({_id: req.params.id}, supplier).then(result => {
-    console.log(result);
-    res.status(200).json({message : "Update Successful !"});
-  });
-});
+// app.put("/api/supplier/:id", (req,res,next)=>{
+//   const supplier = new Supplier({
+//     _id: req.body.id,
+//     supplierID: req.body.supplierID,
+//     name: req.body.name,
+//     email: req.body.email,
+//     contact: req.body.contact,
+//     drugsAvailable: req.body.drugsAvailable
+//   });
+//   Supplier.updateOne({_id: req.params.id}, supplier).then(result => {
+//     console.log(result);
+//     res.status(200).json({message : "Update Successful !"});
+//   });
+// });
 
-app.get("/api/supplier",(req,res,next)=>{
-  Supplier.find().then(documents=>{
-    res.status(200).json({
-      message : 'supplier added sucessfully',
-      suppliers :documents
-    });
-  });
-});
-
-
-app.get("/api/supplier/:id",(req,res,next)=>{
-  Supplier.findById(req.params.id).then(supplier =>{
-    if(supplier){
-      res.status(200).json(supplier);
-    }else{
-      res.status(200).json({message:'suplier not found'});
-    }
-  });
-});
-
-app.delete("/api/supplier/:id", (req, res, next) => {
-  Supplier.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Supplier deleted!' });
-  });
-});
+// app.get("/api/supplier",(req,res,next)=>{
+//   Supplier.find().then(documents=>{
+//     res.status(200).json({
+//       message : 'supplier added sucessfully',
+//       suppliers :documents
+//     });
+//   });
+// });
 
 
+// app.get("/api/supplier/:id",(req,res,next)=>{
+//   Supplier.findById(req.params.id).then(supplier =>{
+//     if(supplier){
+//       res.status(200).json(supplier);
+//     }else{
+//       res.status(200).json({message:'suplier not found'});
+//     }
+//   });
+// });
+
+// app.delete("/api/supplier/:id", (req, res, next) => {
+//   Supplier.deleteOne({ _id: req.params.id }).then(result => {
+//     console.log(result);
+//     res.status(200).json({ message: 'Supplier deleted!' });
+//   });
+// });
+
+app.use("/api/supplier",supplierRoutes);
 
 module.exports = app;
---
+++ backend/routes/supplier.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+
+const Supplier = require('../models/supplier');
+
+router.post("",(req,res,next)=>{
+  const supplier = new Supplier({
+    supplierID: req.body.supplierID,
+    name: req.body.name,
+    email: req.body.email,
+    contact: req.body.contact,
+    drugsAvailable: req.body.drugsAvailable
+  });
+  supplier.save().then(createdSupplier=>{
+  res.status(201).json({
+    message:'Supplier Added Successfully',
+    supplierId : createdSupplier._id
+  });
+
+  });
+
+  });
+
+  router.put("/:id", (req,res,next)=>{
+    const supplier = new Supplier({
+      _id: req.body.id,
+      supplierID: req.body.supplierID,
+      name: req.body.name,
+      email: req.body.email,
+      contact: req.body.contact,
+      drugsAvailable: req.body.drugsAvailable
+    });
+    Supplier.updateOne({_id: req.params.id}, supplier).then(result => {
+      console.log(result);
+      res.status(200).json({message : "Update Successful !"});
+    });
+  });
+
+  router.get("",(req,res,next)=>{
+    Supplier.find().then(documents=>{
+      res.status(200).json({
+        message : 'supplier added sucessfully',
+        suppliers :documents
+      });
+    });
+  });
+
+
+  router.get("/:id",(req,res,next)=>{
+    Supplier.findById(req.params.id).then(supplier =>{
+      if(supplier){
+        res.status(200).json(supplier);
+      }else{
+        res.status(200).json({message:'suplier not found'});
+      }
+    });
+  });
+
+  router.delete("/:id", (req, res, next) => {
+    Supplier.deleteOne({ _id: req.params.id }).then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Supplier deleted!' });
+    });
+  });
+
+  module.exports = router;
