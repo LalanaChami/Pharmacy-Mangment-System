d93635c1be1a2b21d006f66056ef33ee7b2fc02e
LalanaChami <lalanachamika123@gmail.com>
2020-04-23 20:56:44 +0530

    Inventory database connected to the online shop #backend

--- src/app/mainwindow/a-inventory-window/a-shopping-cart-window/a-shopping-cart-items/a-shopping-cart-items.component.ts
+++ src/app/mainwindow/a-inventory-window/a-shopping-cart-window/a-shopping-cart-items/a-shopping-cart-items.component.ts
@@ -1,4 +1,7 @@
+import { InventoryInteractionService } from './../../inventory-interaction.service';
+import { Subscription } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Inventory } from '../../inventory.model';
 
 @Component({
   selector: 'app-a-shopping-cart-items',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AShoppingCartItemsComponent implements OnInit {
 
-  constructor() { }
+  inventorys: Inventory[] = [];
+  isLoading= false;
+  private inventorySubs: Subscription;
+
+  constructor(private inventoryInteractionService: InventoryInteractionService) { }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.inventoryInteractionService.getInventory();
+    this.inventorySubs = this.inventoryInteractionService.getInventoryUpdateListener()
+      .subscribe((posts: Inventory[]) => {
+        this.isLoading = false;
+        this.inventorys = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.inventorySubs.unsubscribe();
   }
 
 }
