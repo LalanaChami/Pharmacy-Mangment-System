7607e25c66e8b8c4da7a6082689408db786efc52
LalanaChami <lalanachamika123@gmail.com>
2020-05-12 00:06:52 +0530

    Inventory backend connected to the PoS component #bacend

--- src/app/mainwindow/a-pointofsale-window/bill-window/bill-item/bill-item.component.ts
+++ src/app/mainwindow/a-pointofsale-window/bill-window/bill-item/bill-item.component.ts
@@ -1,5 +1,8 @@
+import { InventoryInteractionService } from './../../../a-inventory-window/inventory-interaction.service';
+import { Subscription } from 'rxjs';
+import { Inventory } from './../../../a-inventory-window/inventory.model';
 import { Component, OnInit } from '@angular/core';
-import { Flickity } from 'flickity';
+
 
 @Component({
   selector: 'app-bill-item',
@@ -8,30 +11,32 @@ import { Flickity } from 'flickity';
 })
 export class BillItemComponent implements OnInit {
   items: Array<any> =[];
+  searchTerm: string;
+  inventorys: Inventory[] = [];
+  isLoading= false;
+  private inventorySubs: Subscription;
 
-  constructor() {
+
+  constructor(private inventoryInteractionService: InventoryInteractionService) {
     this.items =[
       {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'},
-      {name: 'https://i.ibb.co/L9X6wKM/pharmacare-logo-hori-tagline-2.png'}
 
 
     ]
    }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.inventoryInteractionService.getInventory(null,null);
+    this.inventorySubs = this.inventoryInteractionService.getInventoryUpdateListener()
+      .subscribe((posts: Inventory[]) => {
+        this.isLoading = false;
+        this.inventorys = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.inventorySubs.unsubscribe();
   }
 
 }
