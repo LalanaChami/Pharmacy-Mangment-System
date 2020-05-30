d115c43d8f08571595a2b17fe6b2e9a9a7eb241a
LalanaChami <lalanachamika123@gmail.com>
2020-05-30 11:04:23 +0530

    Inventory backend connected with the Shopping Cart component #ui

--- src/app/mainwindow/a-inventory-window/a-shopping-cart-window/a-shopping-cart-items/a-shopping-cart-items.component.ts
+++ src/app/mainwindow/a-inventory-window/a-shopping-cart-window/a-shopping-cart-items/a-shopping-cart-items.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { InventoryInteractionService } from './../../inventory-interaction.service';
 import { PageEvent } from '@angular/material';
 import { Subscription } from 'rxjs';
@@ -13,12 +14,14 @@ import { Inventory } from '../../inventory.model';
 export class AShoppingCartItemsComponent implements OnInit {
   searchTerm: string;
   inventorys: Inventory[] = [];
+  itemArray: Array<any> =[];
   isLoading= false;
   currentPage= 1;
   totalItems= 10;
   itemsPerPage = 8;
   pageSizeOptions =[8,12,16,20,24];
   private inventorySubs: Subscription;
+  itemNumber: number;
 
   constructor(private inventoryInteractionService: InventoryInteractionService) { }
 
@@ -40,6 +43,13 @@ export class AShoppingCartItemsComponent implements OnInit {
 
   ngOnDestroy() {
     this.inventorySubs.unsubscribe();
+  }
+
+  onAddToCart(itemId:string, name:string , expireDate:string , price:string, form:NgForm ){
+    this.itemArray.push([itemId,name,expireDate,price,form.value.quantityNumber]);
+
+    console.log(this.itemArray);
+    this.itemNumber = this.itemArray.length;
   }
 
 }
