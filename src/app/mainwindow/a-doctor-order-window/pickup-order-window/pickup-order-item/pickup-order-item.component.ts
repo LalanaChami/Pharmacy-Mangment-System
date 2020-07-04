d0fbbf9dbac9308592b29998900c9385f228d529
LalanaChami <lalanachamika123@gmail.com>
2020-07-04 10:14:46 +0530

    Connected pickupOder module and out put data on pickedUpOder table #backend

--- src/app/mainwindow/a-doctor-order-window/pickup-order-window/pickup-order-item/pickup-order-item.component.ts
+++ src/app/mainwindow/a-doctor-order-window/pickup-order-window/pickup-order-item/pickup-order-item.component.ts
@@ -1,3 +1,6 @@
+import { EmailInteractionService } from './../../new-doctor-order-window/email-Interaction.service';
+import { DoctorOderServices } from './../../../a-inventory-window/a-shopping-cart-window/DoctorOderServices.service';
+import { Subscription } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickupOrderItemComponent implements OnInit {
 
-  constructor() { }
+  docPickedUpOders: any[] = [];
+  isLoading= false;
+
+  docPickedUpOderSubs: Subscription;
+
+
+
+  constructor(private doctoderService: DoctorOderServices, private emailInteractionService: EmailInteractionService){}
 
   ngOnInit() {
+    this.isLoading = true;
+    this.doctoderService.getPickedUpDocOders();
+    this.docPickedUpOderSubs = this.doctoderService.getPickedUpDocOdersUpdateListener()
+      .subscribe((posts) => {
+        this.isLoading = false;
+        this.docPickedUpOders = posts;
+      });
   }
+
 
 }
