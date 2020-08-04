45dd2c14e1ef08553457b62944a44fb8f300cb62
LalanaChami <lalanachamika123@gmail.com>
2020-08-04 22:15:16 +0530

    Configured to send the Expired Drug request email #backend

--- src/app/mainwindow/x-expired-dialog-box/x-expired-dialog-box.component.ts
+++ src/app/mainwindow/x-expired-dialog-box/x-expired-dialog-box.component.ts
@@ -1,3 +1,4 @@
+import { EmailInteractionService } from './../a-doctor-order-window/new-doctor-order-window/email-Interaction.service';
 import { NgForm } from '@angular/forms';
 import { Component, OnInit , Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material'
@@ -9,13 +10,37 @@ import { MAT_DIALOG_DATA } from '@angular/material'
 })
 export class XExpiredDialogBoxComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data : any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data : any, public emailInteractionService: EmailInteractionService) { }
 
   ngOnInit() {
   }
 
   onSendEmail(name:string,price:string,email:string,quantity:string,form: NgForm){
-    console.log(name,price,email,quantity,form.value.quantityNumber)
+    console.log(name,price,email,quantity,form.value.quantityNumber);
+
+
+    let user={
+      name : name,
+      email : email,
+      price : price,
+      quantity : quantity,
+      quantityNumber : form.value.quantityNumber,
+
+    }
+    console.log(user);
+
+    this.emailInteractionService.sendEmail("http://localhost:3000/api/inventory/sendmail", user).subscribe(
+      data => {
+        let res:any = data;
+        console.log(
+          `👏 ${user.name} an email has been successfully and the message id is ${res.messageId}`
+        );
+      },
+      err => {
+        console.log(err);
+
+      }
+    );
   }
 
 }
