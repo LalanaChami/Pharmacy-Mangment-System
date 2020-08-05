a9f9defaa8611e31df7ab40c23553b1419ac72e8
LalanaChami <lalanachamika123@gmail.com>
2020-08-05 18:57:42 +0530

    Configured the multi user Authentication in dashboad login #backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -12,6 +12,7 @@ export class AuthService{
   private token : string;
   private tokenTimer : any;
   private authStatusListener  = new Subject<boolean>();
+  userRole :string;
 
 
   constructor(private http: HttpClient, private router: Router ){}
@@ -24,12 +25,16 @@ export class AuthService{
     return this.isAuthenticated;
   }
 
+  getUserRole(){
+    return this.userRole;
+  }
+
   getAuthStatusListener(){
     return this.authStatusListener.asObservable();
   }
 
-  createUser(name: string , contact: string , nic: string ,email: string ,password: string ){
-    const authData :AuthData = {name:name , contact:contact , nic:nic , email:email , password:password};
+  createUser(name: string , contact: string , nic: string ,email: string ,password: string , role : string){
+    const authData  = {name:name , contact:contact , nic:nic , email:email , password:password , role:role};
     this.http.post("http://localhost:3000/api/user/signup",authData)
       .subscribe(response =>{
         console.log(response);
@@ -39,12 +44,13 @@ export class AuthService{
 
   login(email: string, password){
     const authData :AuthData = {name: null , contact: null , nic: null , email: email , password: password};
-    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login",authData)
+    this.http.post<{token: string, expiresIn: number, role :string}>("http://localhost:3000/api/user/login",authData)
       .subscribe(response =>{
         const token= response.token;
         this.token=token;
         if(token){
           const expiresInDuration = response.expiresIn;
+          this.userRole = response.role;
           this.setAuthTimer(expiresInDuration);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
