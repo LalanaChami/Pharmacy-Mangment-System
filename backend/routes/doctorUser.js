e113178ff1bcfd39bca0d4f45de2c3b027cac61c
Lalana Chamika <lalanachamika123@gmail.com>
2020-08-16 07:26:25 +0530

    Added becyptjs

--- backend/routes/doctorUser.js
+++ backend/routes/doctorUser.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const checkDocAuth = require("../middleware/check-docAuth");
 
