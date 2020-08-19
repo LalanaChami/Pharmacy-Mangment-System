2cd66493e5770ee2b2c6a94b6da9eac667653b4b
Lalana Chamika <lalanachamika123@gmail.com>
2020-08-19 16:46:48 +0530

    Modified the image path respective to server.js #deploy

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -18,7 +18,7 @@ const storage =multer.diskStorage({
     if(isValid){
       error = null;
     }
-    cb(error, "backend/images");
+    cb(error, "images");
   },
   filename :(req, file ,cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
