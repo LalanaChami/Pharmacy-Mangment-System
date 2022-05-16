636459aead979959170b0d5b5d18c77810bab835
Tom Strassner <tstrassner@mitre.org>
2022-05-16 15:19:40 -0400

    Modify prepoulated user data
    - Change email and name
    - Generate a hashed password from plaintext
    - Remove unneeded disabling of validation

--- backend/populatedb.js
+++ backend/populatedb.js
@@ -1,21 +1,26 @@
-var mongoose = require('mongoose');
-var User = require('./models/user');
+const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const User = require('./models/user');
 
 const mongoConnString = process.env.MONGODB_CONNSTRING ? process.env.MONGODB_CONNSTRING : 'mongodb://pharmacy-information-root:pharmacy-information-password@localhost:27017?retryWrites=true&w=majority';
 mongoose.connect(mongoConnString, {useNewUrlParser: true , useUnifiedTopology: true});
 
-const user = new User({
-  name: 'alice',
-  contact: '555-555-5555',
-  nic: '1234',
-  email: 'alice@example.com' ,
-  password: '$2a$10$CMRf90.VvXfd9CN04duIi.QvtO.US1Gcdzs3aRJsbWztKBstm8wgy',
-  role: 'pharmacist',
-});
+bcrypt.hash('suzy', 10)
+    .then(hash => {
+      const user = new User({
+        name: 'Suzy Pharmichael',
+        contact: '555-555-5555',
+        nic: '1234',
+        email: 'spharmichael@example.com' ,
+        password: hash,
+        role: 'pharmacist',
+      });
 
-// If we don't disable validation, mongoose-unique-validator will erroneously throw a ValidationError
-// on the User email unique constraint
-user.save({ validateBeforeSave: false })
-  .finally(()=> {
-    mongoose.connection.close()
-  });
+      // If we don't disable validation, mongoose-unique-validator will erroneously throw a ValidationError
+      // on the User email unique constraint
+      user.save()
+        .finally(()=> {
+          mongoose.connection.close();
+        });
+    });
+
