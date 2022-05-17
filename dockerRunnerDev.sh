8a838aa672e9782f4e0821dc30a642aab6862b9c
Tom Strassner <tstrassner@mitre.org>
2022-05-17 15:35:39 -0400

    Add prepoulated db data to dev docker image
    Also remove some extraneous whitespace in the dockerRunnerDev.sh script

--- dockerRunnerDev.sh
+++ dockerRunnerDev.sh
@@ -3,7 +3,7 @@
 # Handle closing application on signal interrupt (ctrl + c)
 trap 'kill $CONTINUOUS_INSTALL_PID $SERVER_PID; gradle --stop; exit' INT
 
-mkdir logs 
+mkdir logs
 # Reset log file content for new application boot
 echo "*** Logs for continuous installer ***" > ./logs/installer.log
 echo "*** Logs for 'npm run start' ***" > ./logs/runner.log
@@ -27,11 +27,11 @@ backend_package_lock_modify_time=$(stat -c %Y backend/package-lock.json)
 while sleep 1
 do
     new_package_modify_time=$(stat -c %Y package.json)
-    new_package_lock_modify_time=$(stat -c %Y package-lock.json)    
+    new_package_lock_modify_time=$(stat -c %Y package-lock.json)
 
     new_backend_package_modify_time=$(stat -c %Y backend/package.json)
-    new_backend_package_lock_modify_time=$(stat -c %Y backend/package-lock.json)   
-    
+    new_backend_package_lock_modify_time=$(stat -c %Y backend/package-lock.json)
+
     if [[ "$package_modify_time" != "$new_package_modify_time" || "$package_lock_modify_time" != "$new_package_lock_modify_time" ]]
     then
         echo "running frontend npm install..."
@@ -54,12 +54,13 @@ do
 
 done )  & CONTINUOUS_INSTALL_PID=$!
 
-# Start server process once initial build finishes  
+# Prepopulate database with demo data
+node backend/populatedb.js
+
+# Start server process once initial build finishes
 ( npm run start | tee ./logs/runner.log ) & SERVER_PID=$!
 
 # Handle application background process exiting
 wait $CONTINUOUS_INSTALL_PID $SERVER_PID
 EXIT_CODE=$?
 echo "application exited with exit code $EXIT_CODE..."
-
-
