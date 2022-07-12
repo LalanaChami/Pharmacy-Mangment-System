b4fc8b98f80c3d0259429d47c2d1cd4576e4ca41
Meng-Chau <mengchau@hotmail.com>
2022-07-12 13:41:56 -0400

    requested changes

--- src/app/mainwindow/a-doctor-order-window/new-doctor-order-window/new-doctor-order-item/etasu-popup/etasu-popup.component.ts
+++ src/app/mainwindow/a-doctor-order-window/new-doctor-order-window/new-doctor-order-item/etasu-popup/etasu-popup.component.ts
@@ -25,7 +25,6 @@ export class EtasuPopupComponent implements OnInit {
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.doctorOrderService.getDocOrders();
     this.isLoading = false;
   }
 
